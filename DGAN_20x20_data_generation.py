23f67de74f6c606e0f6f51da1156fada41b71384
cagataysengor <sengorcagatay@gmail.com>
2025-02-07 21:51:48 +0300

    Update DGAN_20x20_data_generation.py

--- DGAN_20x20_data_generation.py
+++ DGAN_20x20_data_generation.py
@@ -61,7 +61,7 @@ def make_generator_model():
 
 generator = make_generator_model()
 
-noise = tf.random.normal([1, 368])
+noise = tf.random.normal([1, 300])
 generated_image = generator(noise, training=False)
 
 plt.imshow(generated_image[0, :, :, 0], cmap='gray')
